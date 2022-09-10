c1067b688c66dca591aabc3cd8218133b08de392
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-09-09 20:17:35 -0700

    f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**      
  * main - print if the last digit of n is greater than five or is zero or is less than six and not zerp
  * Return: 0
@@ -11,25 +12,22 @@ int main(void)
 {
 
 		int n;
-
+		int l;
 
 
 			srand(time(0));
 
 				n = rand() - RAND_MAX / 2;
-
+				l = n %10; 
 					/* your code goes there */
-				int l;
-
-				l = n %10;
 				
 				if (l > 5)
 				{
 					printf("Last digit of %d is %d and is greater than 5\n", n, l);
 				}
 				else if (l == 0)
-				{
-					printf("Last digit of %d is 0\n", n, l);
+{
+		printf("Last digit of %d is %d and is 0\n", n, l);
 				}
 				else if (l < 0 && l != 0)
 				{
