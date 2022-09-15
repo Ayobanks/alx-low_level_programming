1957b84d9b8832e4e3c2664ffda618e63b892e9c
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-09-14 20:37:59 -0700

    n

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /** 
  * main - prints putchar
- * Return:0
+ * Return:always 0
  */
 int main(void)
 {
 	_putchar("_");
-_putchar("p");
+	_putchar("p");
 	_putchar("u");
 	_putchar("t");
 	_putchar("c");
