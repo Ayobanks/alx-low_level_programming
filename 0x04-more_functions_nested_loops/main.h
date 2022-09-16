2c5f1de897d241d5520448cc17d660f947c19f79
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-09-15 19:19:45 -0700

    more

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
- * main - header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
+ *main - header file for prototypes
+ *void prototypes (void)
+ *int prototypes (int)
  */
 
 int _putchar(char c);
@@ -13,11 +13,11 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-void more_nu,bers(void);
+void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_numbrt(int n);
+void print_number(int n);
 
 #endif /* MAIN_H */
