72e5b90b5902761bfa7556ed635c2ac9ead4b209
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-09-22 12:50:49 -0700

    more

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates two strings.
+ *_strcat - concatenates two the string pointed to by @src to
+ * the end of the string pointed to by @dest
  *@dest: string that will be appended
- *@src: string that will be added
+ *@src: string that will be concatenated upon
  *
  *Return: returns pointer to @dest
  */
