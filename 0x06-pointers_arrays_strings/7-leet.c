baf4cc7b53042f2d4d474f9eb8f5fa5c100bba83
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-09-22 15:57:19 -0700

    more

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,39 +9,18 @@
 
 char *leet(char *str)
 {
-int i = 0;
+int i, j;
 
-while (str[i] != '\0')
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
+
+for (i = 0; *(s + i); i++)
 {
-str[i] = transform(str[i]);
-i++;
-}
-return (str);
-}
-
-/**
- *transform - helper function to map a letter with it's leet encoding
- *@x: char to be encoded
- *
- *Return: the encoded char
- */
-
-char transform(char x)
+for (j = 0; j <= 9; j++)
 {
-char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-int i = 0;
-char replacement = x;
-
-while (i < 8)
-{
-if (x == mapping_low[i] || x == mapping_upper[i])
-{
-replacement = i + '0';
-break;
+if (a[j] == *(s + i))
+*(s + i) = b[j];
 }
-i++;
 }
-
-return (replacement);
+return (s);
 }
