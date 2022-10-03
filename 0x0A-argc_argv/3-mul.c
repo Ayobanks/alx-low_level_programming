6df865ddee48bd7dabd98c00a0358ad0ed6b7f06
Bankole Ayomide <bankoleayomide6@gmail.com>
2022-10-03 04:12:54 -0700

    yay

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to string
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, mull;
+
+	if (argc = 3)
+{
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+mul = a * b;
+printf("%d\n", mul)
+}
+else
+{
+	printf("Error\n");
+}
+return (0);
+}
